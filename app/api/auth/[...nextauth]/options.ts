2de7c42694a1e103c9626b630d32fab8c6167370
shreyaspatil <patil.shreyas@northeastern.edu>
2024-07-31 17:41:53 -0400

    added middleware

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -6,7 +6,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 export const options : NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId : process.env.GOOGLEID as string,
+            clientId : process.env.GOOGLE_ID as string,
             clientSecret: process.env.GOOGLE_SECRET as string,
 
         }),
